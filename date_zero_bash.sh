99cf544e2184ecff8777b8b5134f436fdf528130
3438298980 <85310075+3438298980@users.noreply.github.com>
2022-10-06 01:20:13 +0800

    Update date_zero_bash.sh

--- date_zero_bash.sh
+++ date_zero_bash.sh
@@ -4,6 +4,7 @@ cp /opt/nvidia/nsight-systems/2022.1.3/host-linux-x64/libstdc++.so.6 /usr/lib/x8
 cd /content
 rm -f datetrain.csv
 rm -f date.csv
+rm -f train.bin
 
 wget -c https://github.com/leedavid/leela-chess-to-Chinese-Chess/raw/master/lc0/GGnn_train
 wget -c https://github.com/leedavid/leela-chess-to-Chinese-Chess/raw/master/lc0/GGnn
@@ -12,4 +13,5 @@ mv GGnn_train datetrain.csv
 mv GGnn date.csv
 chmod 777 datetrain.csv
 chmod 777 date.csv
+chmod 777 train.bin
 ./datetrain.csv --user '泽田小言' --password 'zly4444' --lc0name 'date.csv' --logout 0
